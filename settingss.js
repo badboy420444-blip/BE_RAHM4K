9f1913b9c13d6f3511f19101d0d0fcba97d81a84
𓍯‌𝐁𝐞↷𝐑𝐚𝐡𝐦🫩❗ <badboy420444@gmail.com>
2025-08-02 12:51:57 +0430

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~InUUQR6K#Sd8BoErw2USUIgEkLRylARMbCtNNTbzuIvhpozBiJmI",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "93744671474", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
